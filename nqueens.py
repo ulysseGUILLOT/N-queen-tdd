6739b5e72896f13edfe7d206ef0655ecd6e4eaf4
ulysseGUILLOT <ulysseguillot@gmail.com>
2025-02-12 15:10:31 +0100

    test creation plateau de taille N

--- nqueens.py
+++ nqueens.py
@@ -1,4 +1,10 @@
+def create_board(n):
+    return [["0" for _ in range(n)] for _ in range(n)]
+
+
+
 def solve_nqueens(n):
     if n == 1:
         return [["#"]]
     return []
+
--- test_nqueens.py
+++ test_nqueens.py
@@ -1,7 +1,26 @@
 import pytest
-from nqueens import solve_nqueens
+from nqueens import *
 
 def test_nqueens_1():
     result = solve_nqueens(1)
     expected = [["#"]]
     assert result == expected
+
+
+@pytest.mark.parametrize("n, expected", [
+    (1, [["0"]]),
+
+    (2, [["0", "0"],
+         ["0", "0"]]),
+
+    (3, [["0", "0", "0"],
+         ["0", "0", "0"],
+         ["0", "0", "0"]]),
+
+    (4, [["0", "0", "0", "0"],
+         ["0", "0", "0", "0"],
+         ["0", "0", "0", "0"],
+         ["0", "0", "0", "0"]])
+])
+def test_create_board(n, expected):
+    assert create_board(n) == expected
