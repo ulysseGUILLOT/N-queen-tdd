a5b376f1c79cb423cb17e8305243cacdff5be79a
ulysseGUILLOT <ulysseguillot@gmail.com>
2025-02-12 15:33:05 +0100

    modification caractères utilisés dans le plateau

--- nqueens.py
+++ nqueens.py
@@ -1,10 +1,9 @@
 def create_board(n):
-    return [["0" for _ in range(n)] for _ in range(n)]
-
+    return [[0 for _ in range(n)] for _ in range(n)]
 
 
 def solve_nqueens(n):
     if n == 1:
-        return [["#"]]
+        return [[1]]
     return []
 
--- test_nqueens.py
+++ test_nqueens.py
@@ -3,24 +3,24 @@ from nqueens import *
 
 def test_nqueens_1():
     result = solve_nqueens(1)
-    expected = [["#"]]
+    expected = [[1]]
     assert result == expected
 
 
 @pytest.mark.parametrize("n, expected", [
-    (1, [["0"]]),
+    (1, [[0]]),
 
-    (2, [["0", "0"],
-         ["0", "0"]]),
+    (2, [[0, 0],
+         [0, 0]]),
 
-    (3, [["0", "0", "0"],
-         ["0", "0", "0"],
-         ["0", "0", "0"]]),
+    (3, [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]),
 
-    (4, [["0", "0", "0", "0"],
-         ["0", "0", "0", "0"],
-         ["0", "0", "0", "0"],
-         ["0", "0", "0", "0"]])
+    (4, [[0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0]])
 ])
 def test_create_board(n, expected):
     assert create_board(n) == expected
